de524e8781f505c9d98fcead36bfe7f742fdf384
Jonathan Parker <jonathanp62@gmail.com>
2026-08-27 11:13:55 -0400

    WIP commit for the exercises in chapter 3

---
+++ chapter3/src/data_types.rs
@@ -0,0 +1,68 @@
+// (#)data_types.rs 0.1.0   08/27/2026
+//
+// @author   Jonathan Parker
+// @version  0.1.0
+// @since    0.1.0
+//
+// Copyright (c) 2026 by Jonathan Parker.
+
+// SPDX-License-Identifier: MIT
+
+/// The data_types function demonstrates data types
+pub fn data_types() {
+    scalar_types();
+    tabular_types();
+}
+
+/// The scalar_types function demonstrates scalar types
+fn scalar_types() {
+    println!("Scalar types");
+
+    let x = 2.0;   // f64
+    let y: f32 = 3.0;   // f32
+    let z: bool = true; // bool
+    let e: char = '😈'; // char can be any Unicode scalar value
+
+    println!("x = {}, y = {}, z = {}, e = {}", x, y, z, e);
+    
+    numeric_operations();
+}
+
+/// The numeric_operations function demonstrates numeric operations
+fn numeric_operations() {
+    println!("Numeric operations");
+    
+    let sum = 5 + 10;
+    let difference = 95.5 - 4.3;
+    let product = 4 * 30;
+    let quotient = 56.7 / 32.2;
+    let remainder = 43 % 5;
+    
+    println!(
+        "sum = {}, difference = {}, product = {}, quotient = {}, remainder = {}", 
+        sum, 
+        difference, 
+        product, 
+        quotient, 
+        remainder
+    );
+}
+
+/// The tabular_types function demonstrates tabular types
+fn tabular_types() {
+    println!("Tabular (compound) types");
+    
+    tuples();
+    arrays();
+}
+
+/// The tuples function demonstrates tuples
+fn tuples() {
+    println!("Tuples");
+
+}
+
+/// The arrays function demonstrates arrays
+fn arrays() {
+    println!("Arrays");
+}
---
+++ chapter3/src/main.rs
@@ -0,0 +1,32 @@
+// (#)main.rs   0.1.0   08/27/2026
+//
+// @author   Jonathan Parker
+// @version  0.1.0
+// @since    0.1.0
+//
+// Copyright (c) 2026 by Jonathan Parker.
+
+// SPDX-License-Identifier: MIT
+
+//! # Chapter 3 exercises
+//!
+//! ## Execution
+//!
+//! You can run this binary using Cargo:
+//! ```bash
+//! cargo run
+//! ```
+
+mod data_types;
+mod variables;
+
+use data_types::data_types;
+use variables::variables;
+
+/// The main entry point for application execution.
+///
+/// This function is called automatically by the runtime when the binary starts.
+fn main() {
+    variables();
+    data_types();
+}
