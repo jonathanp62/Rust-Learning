4e3904fd003ccafa77f7f8c3036ac70069f7af92
Jonathan Parker <jonathanp62@gmail.com>
2026-08-30 15:40:40 -0400

    Finished up the 12 days of Christmas

--- chapter3/src/exercises.rs
+++ chapter3/src/exercises.rs
@@ -82,21 +82,55 @@ fn compute_nth_fibonacci(n: u32) -> u32 {
 fn twelve_days_of_christmas() {
     println!("Twelve days of Christmas");
 
-    let verses = [
-        "On the first day of Christmas my true love gave to me: a Partridge in a Pear Tree.", 
-        "On the second day of Christmas my true love gave to me: two Turtle Doves, and a Partridge in a Pear Tree.", 
-        "On the third day of Christmas my true love gave to me: three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.", 
-        "On the fourth day of Christmas my true love gave to me: four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.", 
-        "On the fifth day of Christmas my true love gave to me: five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.", 
-        "On the sixth day of Christmas my true love gave to me: six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.", 
-        "On the seventh day of Christmas my true love gave to me: seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.", 
-        "On the eighth day of Christmas my true love gave to me: eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.", 
-        "On the ninth day of Christmas my true love gave to me: nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.", 
-        "On the tenth day of Christmas my true love gave to me: ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.", 
-        "On the eleventh day of Christmas my true love gave to me: eleven Pipers Piping, ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.", 
-        "On the twelfth day of Christmas my true love gave to me: twelve Drummers Drumming, eleven Pipers Piping, ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree."];
+    let on_the = "On the ";
 
-    for verse in verses {
-        println!("{verse}");
+    let days = [
+        "first", 
+        "second", 
+        "third", 
+        "fourth", 
+        "fifth", 
+        "sixth", 
+        "seventh", 
+        "eighth", 
+        "ninth", 
+        "tenth", 
+        "eleventh", 
+        "twelfth"
+    ];
+
+    let day_of_christmas = " day of Christmas my true love gave to me: ";
+
+    let gifts = [
+        "a Partridge in a Pear Tree",
+        "two Turtle Doves",
+        "three French Hens",
+        "four Calling Birds",
+        "five Gold Rings",
+        "six Geese-a-Laying",
+        "seven Swans-a-Swimming",
+        "eight Maids-a-Milking",
+        "nine Ladies Dancing",
+        "ten Lords-a-Leaping",
+        "eleven Pipers Piping",
+        "twelve Drummers Drumming"
+    ];
+
+    for (i, day) in days.iter().enumerate() {
+        print!("{}{}{}{}", on_the, day, day_of_christmas, gifts[i]);
+
+        for j in (0..i).rev() {
+            if j == 0 {
+                print!(", and {}", gifts[j]);
+            } else {
+                print!(", {}", gifts[j]);
+            }
+        }
+
+        println!(".");
+
+        if i < days.len() - 1 {
+            println!();
+        }
     }
 }
