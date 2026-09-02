f60bf7259bb8c141006712296b1d532acd156da4
Jonathan Parker <jonathanp62@gmail.com>
2026-09-02 11:09:31 -0400

    Initial work on enums and pattern matching

---
+++ chapter6/src/enum_values/ip_combo.rs
@@ -0,0 +1,15 @@
+// (#)ip_combo.rs    0.1.0   09/02/2026
+//
+// @author   Jonathan Parker
+// @version  0.1.0
+// @since    0.1.0
+//
+// Copyright (c) 2026 by Jonathan Parker.
+
+// SPDX-License-Identifier: MIT
+
+/// An enum representing different IP address kinds
+pub enum IpCombo {
+    V4(String),
+    V6(String),
+}
---
+++ chapter6/src/main.rs
@@ -0,0 +1,29 @@
+// (#)main.rs   0.1.0   09/02/2026
+//
+// @author   Jonathan Parker
+// @version  0.1.0
+// @since    0.1.0
+//
+// Copyright (c) 2026 by Jonathan Parker.
+
+// SPDX-License-Identifier: MIT
+
+//! # Chapter 6 exercises
+//!
+//! ## Execution
+//!
+//! You can run this binary using Cargo:
+//! ```bash
+//! cargo run
+//! ```
+
+mod enum_values;
+
+use enum_values::enum_values;
+
+/// The main entry point for application execution.
+///
+/// This function is called automatically by the runtime when the binary starts.
+fn main() {
+    enum_values();
+}
