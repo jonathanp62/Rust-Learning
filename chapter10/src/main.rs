54e5fbe1bafc60c186557b38e851d2a3cd655898
Jonathan Parker <jonathanp62@gmail.com>
2026-09-08 07:11:34 -0400

    Initialized chapter 10

---
+++ chapter10/src/main.rs
@@ -0,0 +1,25 @@
+// (#)main.rs   0.1.0   09/08/2026
+//
+// @author   Jonathan Parker
+// @version  0.1.0
+// @since    0.1.0
+//
+// Copyright (c) 2026 by Jonathan Parker.
+
+// SPDX-License-Identifier: MIT
+
+//! # Chapter 10 exercises
+//!
+//! ## Execution
+//!
+//! You can run this binary using Cargo:
+//! ```bash
+//! cargo run
+//! ```
+
+/// The main entry point for application execution.
+///
+/// This function is called automatically by the runtime when the binary starts.
+fn main() {
+    println!("Chapter 10 exercises");
+}
