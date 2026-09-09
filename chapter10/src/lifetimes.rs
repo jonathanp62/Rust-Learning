e911c4939d84ef02e825ccace71fb0c5522ac21c
Jonathan Parker <jonathanp62@gmail.com>
2026-09-09 18:20:51 -0400

    Began working with lifetime annotations

--- chapter10/src/lifetimes.rs
+++ chapter10/src/lifetimes.rs
@@ -12,5 +12,36 @@
 
 /// The lifetimes function
 pub fn lifetimes() {
-    println!("Running lifetimes function");
+    // Variables string1 and string2 have different lifetimes
+    let string1 = String::from("abcd"); // Function-local lifetime (shorter)
+    let string2 = "xyz";                  // String literal ('static')lifetime
+
+    let result = longest(string1.as_str(), string2);
+
+    println!("The longest string is {result}");
+
+    let string1 = String::from("long string is long");
+
+    {
+        let string2 = String::from("xyz");  // Has the shorter lifetime so result can't outlive it
+        let result = longest(string1.as_str(), string2.as_str());
+
+        println!("The longest string is {result}");
+    }
+}
+
+/// The longest function
+/// 'a is a lifetime parameter that ensures the returned reference is valid
+/// for at least as long as the shorter of the two input references.
+/// 
+/// # Arguments
+/// 
+/// * `x` - A string slice
+/// * `y` - A string slice
+/// 
+/// # Returns
+/// 
+/// * `&str` - A string slice
+fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
+    if x.len() > y.len() { x } else { y }
 }
