fac92945cc66f65be9e11c7592f8566f1d00aef7
Jonathan Parker <jonathanp62@gmail.com>
2026-09-10 06:57:42 -0400

    Completed working with lifetimes

--- chapter10/src/lifetimes.rs
+++ chapter10/src/lifetimes.rs
@@ -28,6 +28,8 @@ pub fn lifetimes() {
 
         println!("The longest string is {result}");
     }
+
+    structs();
 }
 
 /// The longest function
@@ -44,4 +46,54 @@ pub fn lifetimes() {
 /// * `&str` - A string slice
 fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
     if x.len() > y.len() { x } else { y }
+}
+
+
+/// The structs function
+/// Demonstrates struct definitions with lifetime parameters
+fn structs() {
+    /// The ImportantExcerpt struct
+    /// 
+    /// # Fields
+    /// 
+    /// * `part` - A reference to a string slice with lifetime 'a
+    struct ImportantExcerpt<'a> {
+        part: &'a str,
+    }
+    
+    /// Implementation of ImportantExcerpt
+    impl<'a> ImportantExcerpt<'a> {
+        /// Returns the level of the excerpt
+        /// 
+        /// # Returns
+        /// 
+        /// * `i32` - The level of the excerpt
+        fn level(&self) -> i32 {
+            3
+        }
+
+        /// Announces and returns the part of the excerpt
+        /// 
+        /// # Arguments
+        /// 
+        /// * `announcement` - A string slice
+        /// 
+        /// # Returns
+        /// 
+        /// * `&str` - A string slice
+        fn announce_and_return_part(&self, announcement: &str) -> &str {
+            println!("Attention please: {announcement}");
+            self.part
+        }
+    }
+
+    let novel = String::from("Call me Ishmael. Some years ago...");
+    let first_sentence = novel.split('.').next().expect("Could not find a '.'");
+    let i = ImportantExcerpt {
+        part: first_sentence,
+    };
+    
+    println!("Important excerpt: {}", i.part);
+    println!("Level: {}", i.level());
+    println!("Announcement: {}", i.announce_and_return_part("Hello"));
 }
