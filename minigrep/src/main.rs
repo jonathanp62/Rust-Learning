a64afc482e958c418519416a690f365b089bbf20
Jonathan Parker <jonathanp62@gmail.com>
2026-09-12 11:39:20 -0400

    Initial handling command line arguments

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -8,7 +8,17 @@
 
 // SPDX-License-Identifier: MIT
 
+use std::env;
+
 /// Main function.
 fn main() {
-    println!("Welcome to minigrep");
+    let args: Vec<String> = env::args().collect();
+
+    dbg!(&args);
+
+    let query = &args[1];
+    let file_path = &args[2];
+
+    println!("Searching for {query}");
+    println!("In file {file_path}");
 }
