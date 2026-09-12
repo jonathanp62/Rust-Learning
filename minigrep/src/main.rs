60d858e7aaf38f74f713813efa1d20d3291a640a
Jonathan Parker <jonathanp62@gmail.com>
2026-09-12 08:19:22 -0400

    Initial commit for minigrep

---
+++ minigrep/src/main.rs
@@ -0,0 +1,14 @@
+// (#)main.rs   0.1.0 09/12/2026
+//
+// @author   Jonathan Parker
+// @version  1.0
+// @since    1.0
+//
+// Copyright (c) 2026 by Jonathan Parker.
+
+// SPDX-License-Identifier: MIT
+
+/// Main function.
+fn main() {
+    println!("Welcome to minigrep");
+}
