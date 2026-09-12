c7414f88ebbdc78bc529ea9e83731987e858a37a
Jonathan Parker <jonathanp62@gmail.com>
2026-09-12 12:34:38 -0400

    Updated the config parser to return a struct

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -17,15 +17,21 @@ fn main() {
 
     dbg!(&args);
 
-    let (query, file_path) = parse_config(&args);
+    let config = parse_config(&args);
 
-    println!("Searching for {query}");
-    println!("In file {file_path}");
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.file_path);
 
-    let contents = fs::read_to_string(file_path)
+    let contents = fs::read_to_string(config.file_path)
         .expect("Should have been able to read the file");
 
     println!("With text:\n{contents}");
+}
+
+/// Configuration for the search.
+struct Config {
+    query: String,
+    file_path: String,
 }
 
 /// Parse command line arguments into query and file path.
@@ -37,9 +43,12 @@ fn main() {
 /// # Returns
 /// 
 /// A tuple containing the query and file path.
-fn parse_config(args: &[String]) -> (&str, &str) {
+fn parse_config(args: &[String]) -> Config {
     let query = &args[1];
     let file_path = &args[2];
 
-    (query, file_path)
+    Config {
+        query: query.clone(),
+        file_path: file_path.clone(),
+    }
 }
