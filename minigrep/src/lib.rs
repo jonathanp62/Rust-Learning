3ea0e6f9fff4a9e6be29069fad9969eb7c5a5e8b
Jonathan Parker <jonathanp62@gmail.com>
2026-09-14 14:25:41 -0400

    Added a new function to search without case sensitivity along with a unit test

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -30,18 +30,59 @@ pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     results
 }
 
+
+/// Search for a query string in a content string, ignoring case.
+/// 
+/// # Arguments
+/// 
+/// * `query` - The query string to search for.
+/// * `contents` - The content string to search in.
+/// 
+/// # Returns
+/// 
+/// A vector of strings that contain the query string, ignoring case.
+pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let query = query.to_lowercase();
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
+            results.push(line);
+        }
+    }
+
+    results
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn one_result() {
+    fn test_case_sensitive() {
         let query = "duct";
         let contents = "\
 Rust:
 safe, fast, productive.
-Pick three.";
+Pick three.
+Duct tape.";
 
         assert_eq!(vec!["safe, fast, productive."], search(query, contents));
     }
+
+    #[test]
+    fn test_case_insensitive() {
+        let query = "rUsT";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+
+        assert_eq!(
+            vec!["Rust:", "Trust me."],
+            search_case_insensitive(query, contents)
+        );
+    }
+
 }
