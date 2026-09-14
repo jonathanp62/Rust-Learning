4c2242d001f74aa1a74265cba21381749dc79a07
Jonathan Parker <jonathanp62@gmail.com>
2026-09-14 13:38:20 -0400

    Implemented a working search function and removed excess debugging messages

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -19,7 +19,15 @@
 /// 
 /// A vector of strings that contain the query string.
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    vec![]
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+
+    results
 }
 
 #[cfg(test)]
--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -19,17 +19,14 @@ use std::process;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    dbg!(&args);
-
     let config = Config::build(&args).unwrap_or_else(|err| {
         println!("Problem parsing arguments: {err}");
         process::exit(1);
     });
 
-    println!("Searching for {}", config.query);
-    println!("In file {}", config.file_path);
+    println!("Searching for '{}' in file {}...", config.query, config.file_path);
 
-    if let Err(e) = run(config) {
+    if let Err(e) = run(config, false) {
         println!("Application error: {e}");
         process::exit(1);
     }
@@ -47,13 +44,15 @@ fn main() {
 /// # Returns
 /// 
 /// A Result indicating success or an error.
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
+fn run(config: Config, debug: bool) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.file_path)?;
 
-    println!("With text:\n{contents}");
+    if debug {
+        println!("With text:\n{contents}");
+    }
 
     for line in search(&config.query, &contents) {
-        println!("{line}");
+        println!(">>>{line}<<<");
     }
 
     Ok(())
